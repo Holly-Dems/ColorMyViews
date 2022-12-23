5badd6874289cafb5bc75ebe5cce1f0ed0778434
Holly-Dems <111143320+Holly-Dems@users.noreply.github.com>
2022-12-23 08:47:22 +0100

    colorsmyview terminé

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -1,12 +1,64 @@
 package com.example.colormyviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+//import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setListeners()
+    }
+
+
+    private fun setListeners(){
+        val box_one_text = findViewById<TextView>(R.id.box_one_text)
+        val box_two_text = findViewById<TextView>(R.id.box_two_text)
+        val box_three_text = findViewById<TextView>(R.id.box_three_text)
+        val box_four_text = findViewById<TextView>(R.id.box_four_text)
+        val box_five_text = findViewById<TextView>(R.id.box_five_text)
+        val constraint_layout = findViewById<ConstraintLayout>(R.id.constraint_layout)
+        val btnred = findViewById<Button>(R.id.red_button)
+        val btnyellow = findViewById<Button>(R.id.yellow_button)
+        val btngreen = findViewById<Button>(R.id.green_button)
+
+        val clickaleViews:List<View> =
+            listOf(box_one_text,box_two_text,box_three_text,box_four_text,box_five_text,constraint_layout,btnred,btngreen,btnyellow)
+        for (item in clickaleViews){
+            item.setOnClickListener{makeColored(it)}
+        }
+    }
+
+    private
+    fun makeColored(view: View) {
+        val box_three_text = findViewById<TextView>(R.id.box_three_text)
+        val box_four_text = findViewById<TextView>(R.id.box_four_text)
+        val box_five_text = findViewById<TextView>(R.id.box_five_text)
+
+        when (view.id) {
+
+            // Boxes using Color class colors for background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
